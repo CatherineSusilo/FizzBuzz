80f5ac485a183e9353b9153309bdb052735a0022
CatherineSusilo <125679773+CatherineSusilo@users.noreply.github.com>
2026-09-14 14:36:56 -0400

    feat: multiples exercise part 1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Multiples {
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int count = multiples();
+        System.out.println(count);
+    }
+}
+    
